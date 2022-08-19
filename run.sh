7d71c960aa78b112aade2ea886ebbc1808b2dcce
frankwy35 <32762885+ywang037@users.noreply.github.com>
2022-08-19 18:04:26 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
-python main_Fed_DC.py --dataset=CIFAR10 --model=ConvNet --ipc=1 --num_exp=5 --num_eval=10 --epoch_eval_train=50 --Iteration=50 --save_results --stand_alone
+python main_Fed_DC.py --dataset=CIFAR10 --model=ConvNet --ipc=1 --num_exp=5 --Iteration=50 --num_eval=10 --epoch_eval_train=50 --server_mode=train --save_results --stand_alone
 # python main_Fed_DC_data.py --dataset=MNIST --model=ConvNet --ipc=1 --num_exp=5 --num_eval=10 --epoch_eval_train=50 --Iteration=50 --save_results --stand_alone
-# python main_Fed_DC2.py --dataset=MNIST --model=ConvNet --ipc=1 --num_exp=5 --num_eval=10 --epoch_eval_train=50 --Iteration=50 --save_results --stand_alone
